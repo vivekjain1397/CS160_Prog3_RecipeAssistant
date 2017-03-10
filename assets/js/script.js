60c64eb5f857eb651976245bf02d8bb27c38b021
ellen <e.hong@berkeley.edu>
2017-03-10 12:41:19 -0800

    started html pages

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	// add new entry to recipe table in home page
+	var new_recipe = "<div class='recipe'>";
+	// get img
+	new_recipe += "<img class='recipe__img' src=";
+	// new_recipe += img
+	new_recipe += "/>";
+	new_recipe += "<div class='recipe__name'>"
+	// get recipe name
+	// new_recipe += recipe name
+	new_recipe += "</div>";
+	// add recipe icons
+	new_recipe += "<a href='ingredients.html'><img class='recipe__icon' src='assets/img/ingredients.png'/></a><a href='directions.html'><img class='recipe__icon' src='assets/img/directions.png'/></a>";
+
+	// on recipe icon click: 
+	// repopulate recipe and ingredients with new info
+
+})
